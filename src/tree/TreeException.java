74539d50f4cecdee616875cb6aab440fdca6e8ea
namanM15 <naman.mishra2034@gmail.com>
2025-04-25 02:15:56 -0700

    Fixed TreeException class update

--- src/tree/TreeException.java
+++ src/tree/TreeException.java
@@ -1,7 +1,7 @@
-package trees;
+package tree;
 
-public class TreeException extends ??? {
+public class TreeException extends Exception {
     public TreeException(String s) {
-        // Pass s into a superclass ctor.
+        super(s);
     }
 }
